3990d362484d7e4eccb6b8fb4d1ca3c4f6641e46
Marko Sanković <sankovic.marko@gmail.com>
2014-10-24 15:04:06 +0200

    Crete BatteryBox

---
+++ app/src/main/java/com/synapticon/bucke/dashboard/BatteryBox.java
@@ -0,0 +1,44 @@
+package com.synapticon.bucke.dashboard;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BatteryBox extends View {
+
+    private Paint mPaint;
+
+    public BatteryBox(Context context) {
+        super(context);
+        init();
+    }
+
+    public BatteryBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BatteryBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.parseColor("#FF29B866"));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Draw rounded rectangle
+        RectF rectF = new RectF(0, 0, getWidth(), getHeight());
+        canvas.drawRoundRect(rectF, 33, 33, mPaint);
+    }
+}
