4ae13ba26dff99d9f417a7f00b4111126397c09b
Marko Sanković <sankovic.marko@gmail.com>
2014-10-25 20:52:08 +0200

    Add text to battery box

--- app/src/main/java/com/synapticon/bucke/dashboard/BatteryBox.java
+++ app/src/main/java/com/synapticon/bucke/dashboard/BatteryBox.java
@@ -5,12 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class BatteryBox extends View {
 
     private Paint mPaint;
+    private TextPaint mBatteryTextPaint;
+    private int mBattery = 100;
 
     public BatteryBox(Context context) {
         super(context);
@@ -31,6 +37,12 @@ public class BatteryBox extends View {
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setColor(Color.parseColor("#FF29B866"));
+
+        mBatteryTextPaint = new TextPaint();
+        mBatteryTextPaint.setARGB(255, 255, 255, 255);
+        mBatteryTextPaint.setTextSize(51.43f);
+        mBatteryTextPaint.setAntiAlias(true);
+        mBatteryTextPaint.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/HelveticaNeueLTStd-Th.otf"));
     }
 
     @Override
@@ -40,5 +52,14 @@ public class BatteryBox extends View {
         // Draw rounded rectangle
         RectF rectF = new RectF(0, 0, getWidth(), getHeight());
         canvas.drawRoundRect(rectF, 22, 22, mPaint);
+
+        canvas.translate(0, 130);
+        new StaticLayout(String.valueOf(mBattery), mBatteryTextPaint, 120, Layout.Alignment.ALIGN_CENTER, 1f, 0f, true).draw(canvas);
+        canvas.restore();
+    }
+
+    public void setBattery(int battery) {
+        this.mBattery = battery;
+        invalidate();
     }
 }
