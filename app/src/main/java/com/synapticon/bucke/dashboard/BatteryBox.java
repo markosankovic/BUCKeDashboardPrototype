757bcfd7836cd331478584862ea349e5da1dc572
Marko Sanković <sankovic.marko@gmail.com>
2014-10-28 18:05:11 +0100

    Use bitmaps instead of the filter color for battery progress bar

--- app/src/main/java/com/synapticon/bucke/dashboard/BatteryBox.java
+++ app/src/main/java/com/synapticon/bucke/dashboard/BatteryBox.java
@@ -5,10 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.text.Layout;
 import android.text.StaticLayout;
@@ -27,7 +24,12 @@ public class BatteryBox extends View {
 
     private int mBatteryStateOfCharge = 100;
 
-    private Bitmap mFrameBitmap;
+    private Bitmap mGreenFrameBitmap;
+    private Bitmap mGreenBackgroundBitmap;
+    private Bitmap mOrangeFrameBitmap;
+    private Bitmap mOrangeBackgroundBitmap;
+    private Bitmap mRedFrameBitmap;
+    private Bitmap mRedBackgroundBitmap;
 
     public BatteryBox(Context context) {
         super(context);
@@ -54,16 +56,28 @@ public class BatteryBox extends View {
         mBatteryTextPaint.setAntiAlias(true);
         mBatteryTextPaint.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/HelveticaNeueLTStd-Th.otf"));
 
-        mFrameBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_frame);
+        mGreenFrameBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_green_frame);
+        mGreenBackgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_green_background);
+        mOrangeFrameBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_orange_frame);
+        mOrangeBackgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_orange_background);
+        mRedFrameBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_red_frame);
+        mRedBackgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.battery_box_red_background);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // Set color to one of: green, orange or red depending on the percentage
-        int color = progressBarColor(mBatteryStateOfCharge);
-        mPaint.setColor(color);
+        Bitmap frameBitmap = mGreenFrameBitmap;
+        Bitmap backgroundBitmap = mGreenBackgroundBitmap;
+
+        if (mBatteryStateOfCharge <= 20) {
+            frameBitmap = mRedFrameBitmap;
+            backgroundBitmap = mRedBackgroundBitmap;
+        } else if (mBatteryStateOfCharge <= 35) {
+            frameBitmap = mOrangeFrameBitmap;
+            backgroundBitmap = mOrangeBackgroundBitmap;
+        }
 
         canvas.save();
 
@@ -71,31 +85,16 @@ public class BatteryBox extends View {
         canvas.clipRect(0, progressBarHeight(getHeight(), mBatteryStateOfCharge), getWidth(), getHeight());
 
         // Draw rounded rectangle to be clipped
-        RectF rectF = new RectF(0, 0, getWidth(), getHeight());
-        canvas.drawRoundRect(rectF, 22, 22, mPaint);
+        canvas.drawBitmap(backgroundBitmap, 0, 0, mPaint);
 
         // Restore canvas so that clipping is not applied to the frame bitmap
         canvas.restore();
 
-        Paint p = new Paint(color);
-        ColorFilter filter = new LightingColorFilter(color, 1);
-        p.setColorFilter(filter);
-
-        canvas.drawBitmap(mFrameBitmap, 0, 0, p);
+        canvas.drawBitmap(frameBitmap, 0, 0, mPaint);
 
         canvas.translate(0, 130);
         new StaticLayout(String.valueOf(mBatteryStateOfCharge), mBatteryTextPaint, 120, Layout.Alignment.ALIGN_CENTER, 1f, 0f, true).draw(canvas);
         canvas.restore();
-    }
-
-    int progressBarColor(int percentage) {
-        if (percentage <= 20) {
-            return colors[2];
-        } else if (percentage <= 35) {
-            return colors[1];
-        } else {
-            return colors[0];
-        }
     }
 
     /**
